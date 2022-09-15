7dfe18355c1a64867149f019b0ac2dac45b53a94
christianLeon <christian.leon@deepdatat.com>
2022-09-14 23:44:15 -0500

    elimina error de red

--- DogMonitor/src/services/health-service.tsx
+++ DogMonitor/src/services/health-service.tsx
@@ -1,6 +1,8 @@
 
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { BASE_URL } from '../constants';
+//import {ToastAndroid } from 'react-native';
+
 
 export namespace HelathService
 {
@@ -8,12 +10,15 @@ export namespace HelathService
     {
         try{
             console.log("Getting health...");
+            //ToastAndroid.show(`GET: ${BASE_URL}/api/services/health`, ToastAndroid.LONG);
             const healthResponse:AxiosResponse  =await axios.get(`${BASE_URL}/api/services/health`);
             console.log(healthResponse.data);
             return true;
         }catch(_error)
         {
+
             console.log("Error getting Health");
+            //ToastAndroid.show(_error.message.toString(), ToastAndroid.LONG);
             console.log(_error);
             console.log(_error.response);
             return false;   
